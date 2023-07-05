0e95733f1abfdd7fb0374329d18c7eab999ef76d
ThatOneStas <138216528+ThatOneStas@users.noreply.github.com>
2023-07-05 12:48:52 +0300

    Add files via upload

---
+++ task_3.py
@@ -0,0 +1,20 @@
+# ---- Завдання перше 1 ----
+
+# Num_1 = int(input("Впишіть перше число: "))
+# Num_2 = int(input("Впишіть друге число: "))
+# Num_3 = int(input("Впишіть третє число: "))
+# print(f"Сумма: {Num_1 + Num_2 + Num_3}")
+# print(f"Добуток: {Num_1 * Num_2 * Num_3}")
+
+# ---- Завдання друге 2 ----
+
+# Month_Salary = int(input('Впишіть вашу місячну зарплату: '))
+# Bank_Payment = int(input('Впишіть сумму банківського платежу: '))
+# Municipal_Debt = int(input('Впишіть вашу комунальну заборгованість: '))
+# print(f"Залишок: {Month_Salary - Bank_Payment - Municipal_Debt} грн.")
+
+# ---- Завдання третє 3 ----
+
+# Diagonal_1 = int(input("Впишіть довжину першої діагоналі: "))
+# Diagonal_2 = int(input("Впишіть довжину другої діагоналі: "))
+# print(f"Площа ромбу: {Diagonal_1 * Diagonal_2 / 2}")
